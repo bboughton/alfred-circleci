1654eff1684be8a085640127e7ebb32b362e1341
Brian Boughton <brian@brianboughton.com>
2017-12-11 09:13:56 -0700

    Log load cache script
    Log output from load_cache script for debugging purposes.

--- load_cache.sh
+++ load_cache.sh
@@ -2,16 +2,22 @@
 # Try loading alfred-circleci cache. If the cache fails to load at first then
 # retry 3 times with an exponetial backoff
 
-function command { 
+log() {
+	echo $(date +%Y%m%dt%H%M%S) "$@"
+}
+
+loadcache() {
 	/usr/local/opt/alfred-circleci/bin/alfred-circleci run loadcache
 }
 
-command && exit
-
-n=1
-until [ $n -ge 4 ]
+log "retrieving projects from circle ci"
+n=0
+while (( $n < 4 ))
 do
-	sleep $((10**$n))
-	command && exit
+	loadcache && exit
 	n=$[$n+1]
+	log "failed to retrieve projects, waiting $((10**n)) seconds"
+	sleep $((10**$n))
 done
+
+exit 1
